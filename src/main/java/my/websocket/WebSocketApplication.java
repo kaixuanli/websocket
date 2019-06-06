4fe50723bba90899dfc88dfc63ee72990e0634f0
kaixuan.li <li.kaixuan@innodev.com.cn>
2019-06-06 15:04:07 +0800

    基于@ServerEndpoint实现wensocket通信

---
+++ src/main/java/my/websocket/WebSocketApplication.java
@@ -0,0 +1,17 @@
+package my.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author likaixuan email:likaixuan(a)innodev.com.cn
+ * @Date: 2019/6/6 11:01
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class,args);
+    }
+}
+
